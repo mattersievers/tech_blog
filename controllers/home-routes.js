d92a3e8bd82b8e1e283d4db383e9ad9dcb181bf0
Matthew Sievers <mattersievers@gmail.com>
2021-09-26 23:53:46 -0500

    homepage loads html. basic models built

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,29 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User} = require('../models');
+
+router.get('/', (req,res) => {
+    console.log(req)
+
+    Post.findAll({
+        attributes: [
+            'id',
+            'blog_title',
+            'created_at'
+        ],
+        include: {
+            model: User,
+            attributes: ['username']
+        }
+    })
+    .then( dbPostData => {
+        //pass single object into homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', posts);
+    })
+    .catch( err => {
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
