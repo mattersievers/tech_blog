6d35dc43aa552c4aa9f6771aebea5a1a608e912b
Matthew Sievers <mattersievers@gmail.com>
2021-10-01 01:33:52 -0500

    added style and some auth

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const { response } = require('express');
+const withAuth = require('../../utils/auth');
+
 
 router.get('/', (req, res) => {
     console.log('====================');
@@ -13,11 +15,20 @@ router.get('/', (req, res) => {
             'created_at'
         ],    
         order: [['created_at', 'DESC']],
-        include: 
+        include: [
+            {
+                model: Comment,
+                attributes: ['id','comment_text','post_id','user_id','created_at'],
+                include:{
+                    model: User,
+                    attributes:['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
             }
+        ]
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -26,11 +37,50 @@ router.get('/', (req, res) => {
         });
 });
 
-router.post('/', (req,res) => {
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes:[
+            'id',
+            'blog_title',
+            'blog_text',
+            'created_at', 
+        ],
+        include:[
+            {
+                model: Comment,
+                attributes: ['id','comment_text','post_id','user_id','created_at'],
+                include:{
+                    model: User,
+                    attributes:['username']
+                }
+            },
+            {
+                model:User,
+                attributes: ['Username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req,res) => {
     Post.create({
         blog_title: req.body.blog_title,
         blog_text: req.body.blog_text,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
         .catch( err => {
